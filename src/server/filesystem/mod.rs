1685312ed2571489eaa961d7bb6e0cdb989566c8
Sherlock Holo <sherlockya@gmail.com>
2020-02-22 17:39:02 +0800

    feat: add chroot function

--- src/server/filesystem/mod.rs
+++ src/server/filesystem/mod.rs
@@ -1,6 +1,13 @@
+use std::env;
+
+use nix::NixPath;
+use nix::unistd;
+
 pub use attr::SetAttr;
 pub use file_handle::FileHandle;
 pub use filesystem::Filesystem;
+
+use crate::Result;
 
 mod dir;
 mod entry;
@@ -10,3 +17,8 @@ mod file_handle;
 mod attr;
 mod filesystem;
 
+fn chroot<P: NixPath>(new_root: P) -> Result<()> {
+    unistd::chroot(&new_root)?;
+
+    Ok(env::set_current_dir("/")?)
+}
